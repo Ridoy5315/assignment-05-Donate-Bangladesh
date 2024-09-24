3c78b1baf0914a54009b4cebdafe116292f6b7e9
Ridoy5315 <ridoy.st99@gmail.com>
2024-09-24 12:42:22 +0800

    the donate function is done for the second and third card

--- js/donation.js
+++ js/donation.js
@@ -1,3 +1,4 @@
+// card one
 document.getElementById("noakhali-donate-button").addEventListener("click", function () {
     const noakhaliInput = getValueById("noakhali-donate-input");
 
@@ -17,5 +18,54 @@ document.getElementById("noakhali-donate-button").addEventListener("click", func
       alert("You typed the wrong number. Please type the correct number.");
     }
   });
+
+  // card two
+document.getElementById("feni-donate-button").addEventListener("click", function () {
+    const feniInput = getValueById("feni-donate-input");
+
+    const feniAmount = getInnerTextById("feni-amount");
+
+    const myAmount = getInnerTextById("my-amount");
+
+    if (feniInput > 0 && feniInput <= myAmount) {
+      const feniNewAmount = addDonate(feniInput, feniAmount);
+
+      const myNewAmount = getMoneyFromAmount(myAmount, feniInput);
+
+      document.getElementById("feni-amount").innerText = feniNewAmount;
+
+      document.getElementById("my-amount").innerText = myNewAmount;
+    } else {
+      alert("You typed the wrong number. Please type the correct number.");
+    }
+  });
+
+  // card three
+document.getElementById("quota-donate-button").addEventListener("click", function () {
+    const quotaInput = getValueById("quota-donate-input");
+
+    const quotaAmount = getInnerTextById("quota-amount");
+
+    const myAmount = getInnerTextById("my-amount");
+
+    if (quotaInput > 0 && quotaInput <= myAmount) {
+      const quotaNewAmount = addDonate(quotaInput, quotaAmount);
+
+      const myNewAmount = getMoneyFromAmount(myAmount, quotaInput);
+
+      document.getElementById("quota-amount").innerText = quotaNewAmount;
+
+      document.getElementById("my-amount").innerText = myNewAmount;
+    } else {
+      alert("You typed the wrong number. Please type the correct number.");
+    }
+  });
+
+
+  // history part
+document.getElementById("history-button").addEventListener("click", function(){
+
+  
+})
 
 
